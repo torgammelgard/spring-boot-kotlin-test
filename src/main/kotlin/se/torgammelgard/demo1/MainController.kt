93fa6d0d03846ee76aa456bbe4b4d635bfba27a3
Tor Gammelgård <tor.gammelgard@gmail.com>
2018-11-28 00:54:00 +0100

    Add properties dep

--- src/main/kotlin/se/torgammelgard/demo1/MainController.kt
+++ src/main/kotlin/se/torgammelgard/demo1/MainController.kt
@@ -1,5 +1,6 @@
 package se.torgammelgard.demo1;
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
@@ -10,9 +11,12 @@ data class Article(val title: String, val id: Long)
 @Controller
 class MainController {
 
+    @Value("\${blog.title}")
+    private lateinit var blogTitle: String
+
     @GetMapping("/")
     fun root(model: Model): String {
-        model["title"] = "Blog"
+        model["title"] = blogTitle
         model["articles"] = listOf(
                 Article("The extended phenotype", 1),
                 Article("A tale of two cities", 2),
--- src/test/kotlin/se/torgammelgard/demo1/Demo1ApplicationTests.kt
+++ src/test/kotlin/se/torgammelgard/demo1/Demo1ApplicationTests.kt
@@ -1,20 +1,28 @@
 package se.torgammelgard.demo1
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import javax.sql.DataSource
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class Demo1ApplicationTests(@Autowired val testRestTemplate: TestRestTemplate) {
 
     @Autowired
     lateinit var dataSource: DataSource
+
+    @Value("\${blog.title}")
+    lateinit var blogTitle: String
 
     @Test
     fun `Assert that context loads`() {
@@ -23,5 +31,8 @@ class Demo1ApplicationTests(@Autowired val testRestTemplate: TestRestTemplate) {
     @Test
     fun `Assert home page title`() {
         val entity = testRestTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<title>$blogTitle</title>")
+        print("$blogTitle")
     }
 }
